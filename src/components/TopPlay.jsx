0ca206aa6990bd7e2761fe720ddd1dc2def7c9de
Kamal Rathore <v-kamal.rathore@foundit.ai>
2022-12-26 17:23:09 +0530

    remove errr

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -29,14 +29,14 @@ const TopChartCard = ({
           className="w-20 h-20 rounded-lg"
         />
         <div className="flex-1 flex flex-col justify-center mx-3">
-          <Link to={`/songs/${song.key}`}>
+          <Link to={`/songs/${song?.key}`}>
             <p className="text-xl font-bold text-white">{song?.title}</p>
           </Link>
-          <Link to={`/artists/${song?.artists[0].adamid}`}>
+         { song?.artists?.length &&<Link to={`/artists/${song?.artists[0]?.adamid}`}>
             <p className="text-base font-bold text-gray-300 mt-1">
               {song?.subtitle}
             </p>
-          </Link>
+          </Link>}
         </div>
       </div>
       <PlayPause
@@ -120,19 +120,19 @@ const TopPlay = () => {
             className="mt-4"
           >
             {topPlay?.map((song) => (
-              <SwiperSlide
+             song?.artists?.length? <SwiperSlide
                 key={song?.key}
                 style={{ width: "25%", height: "auto" }}
                 className="rounded-full shadow-lg animate-slideright"
               >
-                <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <Link to={`/artists/${song?.artists[0]?.adamid}`}>
                   <img
                     src={song?.images.background}
                     alt="name"
                     className="rounded-full w-full object-cover"
                   />
                 </Link>
-              </SwiperSlide>
+              </SwiperSlide>:""
             ))}
           </Swiper>
         </div>
