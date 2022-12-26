f702f9e15339a4aca083e974904a73793ee9bc5a
Kamal Rathore <v-kamal.rathore@foundit.ai>
2022-12-26 18:09:56 +0530

    mobile hidden bug

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ const App = () => {
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row-reverse flex-col">
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
@@ -24,9 +27,6 @@ const App = () => {
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
           </div>
         </div>
       </div>
